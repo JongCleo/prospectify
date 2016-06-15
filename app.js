5661f75aee26ee7c45f04b1d5ef3baf40674717c
Leo Kim <JongCleo@users.noreply.github.com>
2016-06-15 04:56:03 -0400

    everything works now

--- app.js
+++ app.js
@@ -1,119 +1,175 @@
-var Nightmare = require('nightmare');
+//Web scraping dependencies
+var Nightmare = require('nightmare'),
+    Promise = require('q').Promise;
+var nightmare = new Nightmare({show:true})
+
+var json2csv = require('json2csv');
+var fs = require('fs');
+
 //Converter Class
 var Converter = require("csvtojson").Converter;
 var converter = new Converter({});
-
-
 //read from file
 require("fs").createReadStream("./test.csv").pipe(converter);
 
 //end_parsed will be emitted once parsing finished
+
+var first_company, plus_company;
+var theArray = {
+  prospects:[]
+}
+var fields = ['company_name', 'full_name', 'title', 'bio'];
+
 converter.on("end_parsed", function (jsonArray) {
-  for(var i =0; i<jsonArray.length; i++)
-  {
-    var first_company = jsonArray[i]['Company name'];
-    var plus_company = first_company.split(" ").join('+');
+
+  syncLoop(2,
+
+  function(loop){
+    first_company = jsonArray[loop.iteration()]['Company name'];
+    plus_company = first_company.split(" ").join('+');
     console.log(plus_company);
-    evalStats(plus_company);
-    evalBio(plus_company);
-  }
+
+    evalStats(plus_company,
+    function(){
+      // nightmare.proc.disconnect();
+      // nightmare.ended = true;
+      var nightmare = new Nightmare({show:true})
+      loop.next()
+    })
+  },
+
+  function(){
+    exportdata(theArray.prospects, fields);
+    console.log('done')
+  })
 
 });
 
-function evalStats(url)
-
-{
-  new Nightmare()
-
-  .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
-    // .goto('https://www.linkedin.com/start/join')
-    // .wait()
-    // .click('.sign-in-link')
-    // .type('#session_key-login','trevor@trevorsookraj.com')
-    // .type('#session_password-login','Hackdays2016 ')
-    // .click('.btn-primary')
-    // .wait()
-  .goto('https://www.linkedin.com/vsearch/f?type=all&keywords=ecommerce+at+'+url)
-  .wait()
-  .wait()
-  .evaluate(function () {
-    var full_name = [];
-    var title = [];
-
-//where main headline is full name
-    $('.title.main-headline').each(function(i, elem) {
-      full_name[i] = $(this).text();
-      console.log(full_name[i]);
-    });
-
-//where description is title
-    $('.description').each(function(i, elem) {
-      title[i] = $(this).text();
-    });
-
-    return {
-      full_namerc1: full_name[0],
-      titlerc1: title[1]
-    }
-  })
-  .end()
-  .then(function (result) {
-    //output in call back
-    console.log(result)
-  })
-  .catch(function (error) {
-    console.error('Search failed:', error +" Also, here is company name: "+url);
-  });
-}
-
-function evalBio(url)
-{
-  new Nightmare()
-  .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
-  .goto('https://www.linkedin.com/vsearch/f?type=all&keywords=ecommerce+at+'+url)
-  .wait()
-  .wait()
-  .click('.title.main-headline')
-  .wait()
-  .evaluate(function () {
-    var bio = [];
-    if(typeof $('.view-public-profile') != "undefined")
-    {
-      $('.view-public-profile').each(function(i, elem) {
-        bio[0] = $(this).text();
-      });
-
-      return {
-        bio0: bio[0]
-      }
-    }
-  })
-  .end()
-  .then(function (result) {
-    //output in call back
-    console.log(result)
-  })
-  .catch(function (error) {
-    console.error('Search failed:', error+ " Also, here is company name: "+url);
+function exportdata(dataSet, headers) {
+  json2csv( { data: dataSet, fields: headers }, function(err, csv) {
+    if (err) console.log(err);
+    fs.writeFile('./export.csv', csv, function(err) {
+      if (err) throw err;
+      console.log('file saved');
+    })
   });
 }
 
 
-//
-//
-// .goto('https://www.linkedin.com/vsearch/f?type=all&keywords=ecommerce+at+'+url[0]+['\+']+url[1])
-// .click('.title.main-headline')
-// .wait()
-// .evaluate(function () {
-//   var bio = [];
-//   if(typeof $('.view-public-profile') != "undefined")
-//   {
-//     $('.view-public-profile').each(function(i, elem) {
-//       bio[0] = $(this).text();
-//     });
-//
-//     return = {
-//       bio0 : bio[0]
-//     }
-//   }
-// })
+  //TODO: code to check if loggedin
+
+  // .goto('https://www.linkedin.com/start/join')
+  // .wait()
+  // .click('.sign-in-link')
+  // .type('#session_key-login','trevor@trevorsookraj.com')
+  // .type('#session_password-login','Hackdays2016 ')
+  // .click('.btn-primary')
+  // .wait()
+
+  // .end()
+  // .then(function (result) {
+  //   console.log(result)
+  // })
+  // .catch(function (error) {
+  //   console.error('Search failed:', error +" Also, here is company name: "+url);
+  // });
+
+function evalStats(url, callback){
+    Promise.resolve(nightmare
+          .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
+            // .goto('https://www.linkedin.com/start/join')
+            // .wait()
+            // .click('.sign-in-link')
+            // .type('#session_key-login','trevor@trevorsookraj.com')
+            // .type('#session_password-login','Hackdays2016 ')
+            // .click('.btn-primary')
+            // .wait()
+          .goto('https://www.linkedin.com/vsearch/f?type=all&keywords=ecommerce+at+'+url)
+          .wait()
+          .evaluate(function(){
+            var full_name = [];
+            var title = [];
+
+            $('.title.main-headline').each(function(i, elem) {
+              full_name[i] = $(this).text();
+            });
+
+            //where description is title
+            $('.description').each(function(i, elem) {
+              title[i] = $(this).text();
+            });
+
+            return {
+                full_name: full_name[0],
+                title: title[1]
+              }
+          }))
+          .then(function(stuff){
+                console.log(stuff)
+                getBio(stuff, function(){
+                  callback()
+                });
+          });
+}
+
+function getBio(data, callback){
+    Promise.resolve(nightmare
+      .click('.title.main-headline')
+      .wait()
+      .evaluate(function () {
+        var bio = [];
+        if($('.view-public-profile').length >0 )
+        {
+          $('.view-public-profile').each(function(i, elem) {
+            bio[0] = $(this).text();
+          });
+          return bio[0]
+        }
+      }))
+    .then(function(result){
+        console.log(result)
+
+        var theObject = {
+          "company_name":first_company,
+          "full_name": data.full_name,
+          "title": data.title,
+          "bio": result
+        }
+        theArray.prospects.push(theObject);
+
+        callback()
+    })
+}
+
+function syncLoop(iterations, process, exit){
+    var index = 0,
+        done = false,
+        shouldExit = false;
+    var loop = {
+        next:function(){
+            if(done){
+                if(shouldExit && exit){
+                    return exit(); // Exit if we're done
+                }
+            }
+            // If we're not finished
+            if(index < iterations){
+                index++; // Increment our index
+                process(loop); // Run our process, pass in the loop
+            // Otherwise we're done
+            } else {
+                done = true; // Make sure we say we're done
+                if(exit) exit(); // Call the callback on exit
+            }
+        },
+        iteration:function(){
+            return index - 1; // Return the loop number we're on
+        },
+        break:function(end){
+            done = true; // End the loop
+            shouldExit = end; // Passing end as true means we still call the exit callback
+        }
+    };
+    loop.next();
+    return loop;
+}
