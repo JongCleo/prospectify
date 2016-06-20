e7e6a3261563872cba6b07b561e9df0975bb50a7
Leo Kim <JongCleo@users.noreply.github.com>
2016-06-20 16:16:54 -0400

    added consolelogs

--- app.js
+++ app.js
@@ -144,6 +144,7 @@ function googleWrap(daurl, callback){
 
   setTimeout(function () {
     request(options, function (err, res, body) {
+      console.log(body)
             var $ = cheerio.load(body);
             var checkTitle = []
             var elem
