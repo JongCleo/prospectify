01a40bc292f23d82da689aca7b147fd2c17f59ec
Leo Kim <JongCleo@users.noreply.github.com>
2016-06-20 15:49:22 -0400

    app now exports to google drive:

--- app.js
+++ app.js
@@ -13,9 +13,8 @@ var fs = require('fs');
 
 var Converter = require("csvtojson").Converter;
 var converter = new Converter({});
-require("fs").createReadStream("demand.csv").pipe(converter);
+require("fs").createReadStream("magneto.csv").pipe(converter);
 
-var fs = require('fs');
 var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
@@ -23,10 +22,9 @@ var googleAuth = require('google-auth-library');
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/drive-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-    process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'drive-nodejs-quickstart.json';
+var SCOPES = ['https://www.googleapis.com/auth/drive.file'];
+var TOKEN_DIR = '.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'drive-nodejs-app.json';
 /////////////////////////////////////////////////////////////
 
 var first_company, plus_company;
@@ -38,7 +36,7 @@ var fields = ['company_name', 'full_name', 'title', 'bio'];
 
 converter.on("end_parsed", function (jsonArray) {
 
-  syncLoop(2,
+  syncLoop(3,
 
   function(loop){
     first_company = jsonArray[loop.iteration()]['Company name'].toLowerCase().replace('llc','').replace('inc','').split(" ").join('+');
@@ -53,6 +51,16 @@ converter.on("end_parsed", function (jsonArray) {
 
   function(){
     exportdata(theArray.prospects, fields);
+    // Load client secrets from a local file.
+    fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+      if (err) {
+        console.log('Error loading client secret file: ' + err);
+        return;
+      }
+      // Authorize a client with the loaded credentials, then call the
+      // Drive API.
+      authorize(JSON.parse(content), uploadFile);
+    });
     nightmare.proc.disconnect();
     nightmare.proc.kill();
     nightmare.ended = true;
@@ -60,8 +68,6 @@ converter.on("end_parsed", function (jsonArray) {
   })
 
 });
-
-
 
 
 function findLink(url, callback){
@@ -88,9 +94,18 @@ function findLink(url, callback){
           .wait()
           .evaluate(function (){
 
-            return {
-              full_name: $('.full-name').text(),
-              title: $('.title').text()
+            if  (!$('.full-name'))
+            {
+              return {
+                full_name: "page not found",
+                title: "page not found"
+              }
+            }
+            else {
+              return {
+                full_name: $('.full-name').text(),
+                title: $('.title').text()
+              }
             }
 
           }))
@@ -104,6 +119,7 @@ function findLink(url, callback){
               "bio": bio
             }
             //console.log(theObject)
+            console.log(theObject)
             theArray.prospects.push(theObject);
 
             callback()
@@ -118,6 +134,14 @@ function googleWrap(daurl, callback){
           'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16'
       }
   }
+
+  var options2 = {
+      url:  "https://www.google.ca/search?q=marketing+at+"+daurl+"+linkedin",
+      headers: {
+          'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16'
+      }
+  }
+
   setTimeout(function () {
     request(options, function (err, res, body) {
             var $ = cheerio.load(body);
@@ -144,10 +168,43 @@ function googleWrap(daurl, callback){
 
                 break;
               }
+            }
 
+            if(!profileLink){
+              request(options2, function (err, res, body) {
+                      var $ = cheerio.load(body);
+                      var checkTitle = []
+                      var elem
+                      var endselector, bioselector;
+                      var bio;
+                      var profileLink;
+
+                      $(".f.slp").each(function(n){
+                        checkTitle[n] = $(this).text().toLowerCase()
+                      })
+
+                      for (var i = 0; i < checkTitle.length; i ++)
+                      {
+                        if( ((checkTitle[i].indexOf("commerce") > -1) || (checkTitle[i].indexOf("marketing") > -1) || (checkTitle[i].indexOf("digital") > -1) || (checkTitle[i].indexOf("chief technology") > -1) || (checkTitle[i].indexOf("founder") > -1)) && (checkTitle[i].indexOf(first_company.split("+").join(' ').replace("\’", "\'")) > -1))
+                        {
+
+                          baseselector =".f.slp:eq("+i+")";
+
+                          bio = find($, baseselector).parent().children().first().children().first().text()
+                          elem = find($, baseselector).parent().parent().children().first().children().attr('href')
+                          profileLink = find($, baseselector).parent().parent().children().first().children().attr('href').substring(7, elem.indexOf('&'))
+
+                          break;
+                        }
+
+                      }
+              })
             }
             callback(bio, profileLink)
     })
+
+
+
   }, 9000)
 
  }
@@ -155,7 +212,7 @@ function googleWrap(daurl, callback){
  function exportdata(dataSet, headers) {
    json2csv( { data: dataSet, fields: headers }, function(err, csv) {
      if (err) console.log(err);
-     fs.writeFile('demandexport.csv', csv, function(err) {
+     fs.writeFile('magentoexport.csv', csv, function(err) {
        if (err) throw err;
        console.log('file saved');
      })
@@ -193,4 +250,106 @@ function syncLoop(iterations, process, exit){
     };
     loop.next();
     return loop;
+}
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
+
+function uploadFile(auth){
+  var drive = google.drive('v3');
+
+  /// convert csv to google spread spreadsheet
+  var fileMetadata = {
+    'name': 'MagentoCsv',
+    'mimeType': 'application/vnd.google-apps.spreadsheet'
+  };
+  var media = {
+    mimeType: 'text/csv',
+    body: fs.createReadStream('magentoexport.csv')
+  };
+  drive.files.create({
+     resource: fileMetadata,
+     media: media,
+     fields: 'id',
+     auth: auth
+  }, function(err, file) {
+    if(err) {
+      // Handle error
+      console.log(err);
+    } else {
+      console.log('File Id:' , file.id);
+    }
+  });
+
 }
